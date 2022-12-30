a833bfc9002eaa76b0219924779cfbe2af7576f7
olawale-o <omoogunolawale@gmail.com>
2022-12-30 13:33:30 +0100

    Implement guide store

--- src/stores/guide.js
+++ src/stores/guide.js
@@ -1,4 +1,31 @@
 import { defineStore } from 'pinia'
 
-const guide_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/fetchAllGuides'
+const guide_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/guides';
 
+export const useGuideStore = defineStore({
+  id: 'guide',
+  state: () => ({
+    guides: [],
+    loading: false,
+    error: null
+  }),
+  actions: {
+    async fetchAllGuides() {
+      this.guides = []
+      this.loading = true
+      try {
+        this.guides = await fetch(
+          guide_endpoint, {
+          method: 'POST',
+        })
+        .then((response) => response.json())
+        .then((data) => data)
+      } catch (error) {
+        console.log(error);
+        this.error = error
+      } finally {
+        this.loading = false
+      }
+    },
+  }
+})
